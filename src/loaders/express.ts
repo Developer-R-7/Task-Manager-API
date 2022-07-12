9d7d456fdf7496973061f4cc4451e173a5ba0ed5
rushi patel <rushinasa06@gmail.com>
2022-07-12 10:22:55 +0530

    :tada: intial commit

---
+++ src/loaders/express.ts
@@ -0,0 +1,34 @@
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import config from '../config';
+import routes from '../api';
+
+export default ({ app }: { app: express.Application }): void => {
+  app.get('/healthcheck', (req, res) => {
+    const healthcheck = {
+      uptime: process.uptime(),
+      message: 'OK',
+      timestamp: Date.now(),
+    };
+    try {
+      return res.json(healthcheck);
+    } catch (e) {
+      return res.status(503).send();
+    }
+  });
+
+  app.enable('trust proxy');
+
+  // Middleware that helps secure app by setting headers
+  app.use(helmet());
+
+  // Enable Cross Origin Resource Sharing to all origins by default
+  app.use(cors());
+
+  // Middleware that transforms the raw string of req.body into json
+  app.use(express.json());
+
+  // Load API routes
+  app.use(config.api.prefix, routes());
+};
